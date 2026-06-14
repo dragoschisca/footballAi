fdfae3006875be50d89f85cbb38ba553631a8c28
Dragos Chisca <dragoschisca@gmail.com>
2026-06-14 13:12:13 +0300

    Initial commit

---
+++ src/main/java/com/docai/demo/DocAiApplication.java
@@ -0,0 +1,13 @@
+package com.docai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docai/demo/DocAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docai.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
